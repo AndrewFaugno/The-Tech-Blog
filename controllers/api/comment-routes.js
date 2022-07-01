fedd2963f73abf828aec3eb206f4c1d6ae8944ec
Andrew Faugno <andrewfaugno825@gmail.com>
2022-06-30 22:48:24 -0400

    created get, post, and delete routes for comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,2 +1,53 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
+
+// get all comments
+router.get('/', (req, res) => {
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// create a comment
+router.post('/', (req, res) => {
+    // check session
+    Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        user_id: req.body.user_id
+        // req.body.user_id to be turned into req.session.user_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+    // if (req.session) {
+        // code to be moved here once session is enabled
+    // }
+});
+
+// delete a comment
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(400).json({ message: 'No comment found with this id' });
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
