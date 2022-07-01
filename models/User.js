0e4bdf39d0757ea61fc87926cb3bec569393f28c
Andrew Faugno <andrewfaugno825@gmail.com>
2022-06-30 22:55:29 -0400

    created password hashing

--- models/User.js
+++ models/User.js
@@ -4,7 +4,10 @@ const bcrypt = require('bcrypt');
 
 // Create the user model
 class User extends Model {
-
+    // setup method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
 }
 
 // define table columns and configuration
@@ -38,6 +41,18 @@ User.init(
         }
     },
     {
+        hooks: {
+            // setup beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
